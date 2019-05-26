b1ec4d31a7addbbc9a2727f46bc1b1216586692f
BIGW3NDY <380769913@qq.com>
2019-05-26 12:47:44 +0800

    fisrt commit

--- main/main.js
+++ main/main.js
@@ -1,4 +1,68 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+module.exports = function main(num) {
+    var arrayForNum0 = ["._.",
+	                    "|.|",
+						"|_|"];
+						
+	var arrayForNum1 = ["...",
+	                    "..|",
+						"..|"];
+						
+	var arrayForNum2 = ["._.",
+	                    "._|",
+						"|_."];
+						
+	var arrayForNum3 = ["._.",
+	                    "._|",
+						"._|"];
+
+	var arrayForNum4 = ["...",
+	                    "|_|",
+						"..|"];
+
+	var arrayForNum5 = ["._.",
+	                    "|_.",
+						"._|"];
+
+	var arrayForNum6 = ["._.",
+	                    "|_.",
+						"|_|"];
+
+	var arrayForNum7 = ["._.",
+	                    "..|",
+						"..|"];
+
+	var arrayForNum8 = ["._.",
+	                    "|_|",
+						"|_|"];
+
+	var arrayForNum9 = ["._.",
+	                    "|_|",
+						"..|"];	
+
+	var numToLCD = [arrayForNum0,
+					arrayForNum1, 
+					arrayForNum2,
+					arrayForNum3,
+					arrayForNum4,
+					arrayForNum5,
+					arrayForNum6,
+					arrayForNum7,
+					arrayForNum8,
+					arrayForNum9]
+					
+	var numArr = num.split("");
+	
+	var ans = "";
+	for(var i=0; i<3; i++){
+		for(var j=0; j<numArr.length; j++){
+			if(j==numArr.length-1){
+				ans = ans+numToLCD[numArr[j]][i]+"\n";
+			}else {
+				ans = ans+numToLCD[numArr[j]][i]+" ";
+			}
+		}
+	}
+	console.log(ans);
+
+    return ans;
 };
